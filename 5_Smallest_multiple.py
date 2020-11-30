d8689c649ecb794e9d7429bc74abf1a94ae5835c
yagana <yagamirina95@gmail.com>
2020-11-30 16:23:37 +0100

    Added ex 5

---
+++ 5_Smallest_multiple.py
@@ -0,0 +1,11 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+#So 2520 is just 1*5*7*8*9
+#Then the smallest positive number will be 1*5*7*9*11*13*16*17*19
+
+print(1*5*7*9*11*13*16*17*19)
+#That's because we have to find the biggest multiples of each number so the
+#result is divisible by all of them.
